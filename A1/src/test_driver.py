59f24e0ad98fb685bd73b68ccd5e1bc6defb5d87
Bhavna Pereira <pereib4@mcmaster.ca>
2021-01-20 18:23:43 -0500

    Created objects to use for troubleshooting complex_adt and triangle_adt

--- A1/src/test_driver.py
+++ A1/src/test_driver.py
@@ -1,5 +1,28 @@
 ## @file test_driver.py
-#  @author 
-#  @brief 
-#  @date 
+#  @author Bhavana Pereira
+#  @brief The following aim to test the validity of the following files: triangle_adt, complex_T
+#  @date 20/01/2021
+
+
+import pytest
+from triangle_adt import TriangleT, TriType
+from complex_adt import ComplexT
+
+
+validInput1 = ComplexT(2, 3)
+validInput2 = ComplexT(3, 9)
+compEdgeCase1 = ComplexT(0, 0)
+compEdgeCase2 = ComplexT(-1, 0)
+
+validIsosceles = TriangleT(1,1,2)
+validScalene = TriangleT(1,2,3)
+validRight = TriangleT(3,4,5)
+validEquilat = TriangleT(3,3,3)
+triEdgeCase1 = TriangleT(0, 1, 2)
+triEdgeCase2 = TriangleT(0.5, 1, 1.5)
+triEdgeCase2 = TriangleT(-3, 4, 5)
+
+
+
+
 
