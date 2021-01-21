6d87cbe93d28853c1d7da7f457cb62fb012b195f
Bhavna Pereira <pereib4@mcmaster.ca>
2021-01-21 17:38:05 -0500

    began commenting

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -11,7 +11,13 @@ class ComplexT:
 		self.__y = y
 
 	def __str__(self):
-		return "coordinates: ({}, {})".format(self.__x, self.__y)
+		return "({}, {})".format(self.__x, self.__y) 
+
+	def __eq__(self, complexT):
+		if complexT.real() == self.__x and complexT.imag() == self.__y:
+			return True
+		else:
+			return False
 
 	def real(self):
 		return self.__x
@@ -19,7 +25,7 @@ class ComplexT:
 	def imag(self):
 		return self.__y
 
-	def r(self):
+	def get_r(self):
 		return abs(sqrt(x**2 + y**2))
 
 	def get_phi(self):
@@ -27,10 +33,7 @@ class ComplexT:
 		return cmath.phase(comp)
 
 	def equal(self, complexT):
-		if complexT.real() == self.__x and complexT.imag() == self.__y:
-			return True
-		else:
-			return False
+		return self.__eq__(complexT)
 
 	def conj(self):
 		return ComplexT(self.__x, -self.__y)
@@ -50,17 +53,18 @@ class ComplexT:
 
 
 	def div(self, complexT):
-		return complex((self.__x / complexT.real()), (self.__y / complexT.imag()))
+		if complexT.real() == 0 or complexT.imag() == 0:
+			raise ZeroDivisionError("Cannot Divide By Zero")
+		else:
+			return ComplexT((self.__x / complexT.real()), (self.__y / complexT.imag()))
 
 	def sqrt(self):
 		cmath.sqrt(self.__x + self.__y*1j)
 
-test = ComplexT(2,3)
-test2 = ComplexT(0,0)
-testn = ComplexT(3,9)
-print (test.real())
-print (test2.conj())
-print (testn.recip())
+
+compEdgeCase1 = ComplexT(0,0)
+expectedReal1 = 0
+print (compEdgeCase1.real() == expectedReal1)
 
 
 
