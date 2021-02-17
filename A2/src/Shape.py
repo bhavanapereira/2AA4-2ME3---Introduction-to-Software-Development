db84ca6c90c5d0371aa1b9a65059e977214e69fa
Bhavna Pereira <pereib4@mcmaster.ca>
2021-02-16 19:56:04 -0500

    implementation and troubleshooting

--- A2/src/Shape.py
+++ A2/src/Shape.py
@@ -2,3 +2,24 @@
 #  @author
 #  @brief
 #  @date
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def cm_x(self):
+        pass
+
+    @abstractmethod
+    def cm_y(self):
+        pass
+   
+    @abstractmethod
+    def mass(self):
+        pass
+
+    @abstractmethod
+    def m_inert(self):
+        pass
