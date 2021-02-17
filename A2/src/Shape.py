bf12c3580f4385aff1cc337f84f828552ab9c191
Bhavna Pereira <pereib4@mcmaster.ca>
2021-02-16 22:59:51 -0500

    Final

--- A2/src/Shape.py
+++ A2/src/Shape.py
@@ -7,29 +7,31 @@ from abc import ABC, abstractmethod
 
 ## @brief The class outlines shape properties
 #  @details The class contains four methods to detail the x and y coordinates
-#  of the center of mass, along with the mass and the m_inert
-class Shape(ABC):
+#           of the center of mass, along with the mass and the m_inert
 
-## @brief Abstract method to pass the x coordinate of the center of mass
-#  @details defines and passes on this property for use by other modules
+
+class Shape(ABC):
+    ## @brief Abstract method to pass the x coordinate of the center of mass
+    #  @details defines and passes on this property for use by other modules
+
     @abstractmethod
     def cm_x(self):
         pass
 
-## @brief Abstract method to pass the y coordinate of the center of mass
-#  @details defines and passes on this property for use by other modules
+    ## @brief Abstract method to pass the y coordinate of the center of mass
+    #  @details defines and passes on this property for use by other modules
     @abstractmethod
     def cm_y(self):
         pass
- 
-## @brief Abstract method to pass the mass of the shape
-#  @details defines and passes on this property for use by other modules  
+
+    ## @brief Abstract method to pass the mass of the shape
+    #  @details defines and passes on this property for use by other modules
     @abstractmethod
     def mass(self):
         pass
-        
-## @brief Abstract method to pass the maoment of inertia
-#  @details defines and passes on this property for use by other modules  
+
+    ## @brief Abstract method to pass the maoment of inertia
+    #  @details defines and passes on this property for use by other modules
     @abstractmethod
     def m_inert(self):
         pass
