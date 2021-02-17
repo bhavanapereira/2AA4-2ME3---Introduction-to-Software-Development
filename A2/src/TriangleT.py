54731dcf1c763d98696097c2450590e7b8d3f1bf
Bhavna Pereira <pereib4@mcmaster.ca>
2021-02-16 22:58:40 -0500

    Final

--- A2/src/TriangleT.py
+++ A2/src/TriangleT.py
@@ -7,46 +7,47 @@ from Shape import *
 
 ## @brief The following class outlines properties of a triangle in motion
 #  @details The properties include x and y coordinate of Center of Mass,
-#  the mass, and the moment of inertia
-class TriangleT(Shape):
+#           the mass, and the moment of inertia
 
-## @brief This method is a constructor for CircleT
-#  @details The method ensures that the radius and mass
-#  are greater than zero, and if they are, the x and y
-#  coordinates, along with the radius and mass are stated
-#  @param the method takes in four natural numbers x,y,s,m 
-#  to represent the x coordinate, the y coordinate, the sidelengths
-#  and the mass, respectively
+
+class TriangleT(Shape):
+    ## @brief This method is a constructor for TriangleT
+    #  @details The method ensures that the radius and mass
+    #           are greater than zero, and if they are, the x and y
+    #           coordinates, along with the radius and mass are stated
+    #  @param x represents the x coordinate of the center of mass (Real Number)
+    #  @param y represents the y coordinate of the center of mass (Real Number)
+    #  @param s represents the sidelength of the triangle (Real Number)
+    #  @param m represents the mass of the objet (Real Number)
     def __init__(self, x, y, s, m):
         if s < 0 or m < 0:
-            raise ValueError
+            raise ValueError('Invalid Input')
         else:
             self.__x = x
             self.__y = y
             self.__s = s
             self.__m = m
 
-## @brief this method returns the value of the x coordinate of the triangle's
-#  center of mass
-#  @returns Returns the state variable of the correct type
+    ## @brief this method returns the value of the x coordinate of the triangle's
+    #         center of mass
+    #  @returns Returns the state variable of the correct type
     def cm_x(self):
         return self.__x
 
-## @brief this method returns the value of the y coordinate of the triangle's
-#  center of mass
-#  @returns Returns the state variable of the correct type
+    ## @brief this method returns the value of the y coordinate of the triangle's
+    #         center of mass
+    #  @returns Returns the state variable of the correct type
     def cm_y(self):
         return self.__y
 
-## @brief this method returns the value of the mass of the triangle
-#  @returns Returns the state variable of the correct type
+    ## @brief this method returns the value of the mass of the triangle
+    #  @returns Returns the state variable of the correct type
     def mass(self):
         return self.__m
 
-## @brief this method calculates the moment of inertia of the triangle
-#  @details using the mass, multiplied by the square of the sidelength
-#  and dividind the product by 12, this method returns the value of the
-#  moment of inertia
+    ## @brief this method calculates the moment of inertia of the triangle
+    #  @details using the mass, multiplied by the square of the sidelength
+    #           and dividind the product by 12, this method returns the value of
+    #           the moment of inertia
     def m_inert(self):
-        return (self.__m * (self.__s**2))/12
-
+        return (self.__m * (self.__s**2)) / 12
