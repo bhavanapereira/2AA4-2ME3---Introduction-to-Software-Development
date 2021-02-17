cf21304de34366a2b63bd43b9f864f80e80d45c7
Bhavna Pereira <pereib4@mcmaster.ca>
2021-02-16 22:59:00 -0500

    final

--- A2/src/BodyT.py
+++ A2/src/BodyT.py
@@ -9,65 +9,64 @@ from Shape import *
 ## @brief The following class outlines properties of all other shapes in motion
 #  @details The properties include x and y coordinate of Center of Mass,
 #  the mass, and the moment of inertia
-class BodyT(Shape):
 
-## @brief This method is a constructor for BodyT
-#  @details The method ensures that all masses in sequence
-#  are greater than zero, and if the length of the sequences
-#  of all parameters are equal. If they are, the x and y
-#  coordinates and the mass are stated using local functions
-#  @param the method takes in three natural numbers x,y,m 
-#  to represent the x coordinate, the y coordinate,
-#  and the mass, respectively
+
+class BodyT(Shape):
+    ## @brief This method is a constructor for BodyT
+    #  @details The method ensures that all masses in sequence
+    #  are greater than zero, and if the length of the sequences
+    #  of all parameters are equal. If they are, the x and y
+    #  coordinates and the mass are stated using local functions
+    #  @param x represents the x coordinate of the center of mass (Real Number)
+    #  @param y represents the y coordinate of the center of mass (Real Number)
+    #  @param m represents the mass of the object (Real Number)
     def __init__(self, x, y, m):
         for i in m:
             if i < 0:
-                raise ValueError
+                raise ValueError('Invalid Input')
         if len(x) == len(y) and len(y) == len(m):
             self.__cmx = cm(x, m)
             self.__cmy = cm(y, m)
             self.__m = sum(m)
-            self.__moment = mmom(x, y, m) - sum(m)*(cm(x, m)**2 + cm(y, m)**2)
+            self.__moment = mmom(x, y, m) - sum(m) * (cm(x, m)**2 + cm(y, m)**2)
         else:
-            raise ValueError
+            raise ValueError('Invalid Input')
 
-## @brief this method returns the value of the x coordinate of the shape's
-#  center of mass
-#  @returns Returns the state variable of the correct type
+    ## @brief this method returns the value of the x coordinate of the shape's
+    #         center of mass
+    #  @returns Returns the state variable of the correct type
     def cm_x(self):
         return self.__cmx
 
-## @brief this method returns the value of the y coordinate of the shape's
-#  center of mass
-#  @returns Returns the state variable of the correct type
+    ## @brief this method returns the value of the y coordinate of the shape's
+    #         center of mass
+    #  @returns Returns the state variable of the correct type (Real Number)
     def cm_y(self):
         return self.__cmy
 
-## @brief this method returns the value of the mass of the shape
-#  @returns Returns the state variable of the correct type
+    ## @brief this method returns the value of the mass of the shape
+    #  @returns Returns the state variable of the correct type
     def mass(self):
         return self.__m
 
-## @brief this method calculates the moment of inertia of the triangle
-#  @details this method returns a value based on the calculated moment of
-#  inertia through local function mmom(x,y,z)
+    ## @brief this method calculates the moment of inertia of the triangle
+    #  @details this method returns a value based on the calculated moment of
+    #           inertia through local function mmom(x,y,z)
     def m_inert(self):
         return self.__moment
 
-def cm(z,m):
+
+def cm(z, m):
     total = 0
     for i in range(0, len(m)):
-        val = z[i]*m[i]
+        val = z[i] * m[i]
         total += val
-    return total/sum(m)
+    return total / sum(m)
 
 
-def mmom(x,y,m):
+def mmom(x, y, m):
     total = 0
     for i in range(0, len(m)):
-        val = m[i]*(x[i]**2 + y[i]**2)
+        val = m[i] * (x[i]**2 + y[i]**2)
         total += val
-    return total 
-
-
-
+    return total
