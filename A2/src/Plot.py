f2df389cf0fa9a1100525bba53096e63d9429cb9
Bhavna Pereira <pereib4@mcmaster.ca>
2021-02-16 19:54:16 -0500

    troubleshooting

--- A2/src/Plot.py
+++ A2/src/Plot.py
@@ -3,3 +3,27 @@
 #  @brief
 #  @date
 #  @details
+
+import matplotlib.pyplot as plt
+
+
+def plot(w,t):
+    if len(w) != len(t):
+        raise ValueError
+    else:
+        fig, axs = plt.subplots(3)
+        fig.suptitle('Motion Simulation')
+        axs[0].set(ylabel = 'x(m)')
+        axs[1].set(ylabel = 'y(m)')
+        axs[2].set(ylabel = 'y(m)')
+        axs[2].set(xlabel = 'x(m)')
+        x = []
+        y = []
+        for i in range(0, len(w)):
+            x.append(w[i][0])
+        for i in range(0, len(w)):
+            y.append(w[i][1])
+        axs[0].plot(x, t)
+        axs[1].plot(y, t)
+        axs[2].plot(y, x)
+        plt.show()
