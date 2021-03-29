e18516607babb974b88c661438deffa9a745c87b
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-29 16:21:24 -0400

    Implementation of Services class

--- A3/src/Services.java
+++ A3/src/Services.java
@@ -1,1 +1,21 @@
 package src;
+
+
+public class Services{
+
+	public static double[] normal(double[] v){
+		double[] arr = new double[v.length];
+		for (int j = 0; j < v.length; j++){
+			arr[j] = v[j] / sum(v);
+		}
+		return arr;
+	}
+
+	private static double sum(double[] v){
+		double sum = 0;
+		for (int i = 0; i < v.length; i++){
+			sum += v[i];
+		}
+		return sum;
+	}
+}
