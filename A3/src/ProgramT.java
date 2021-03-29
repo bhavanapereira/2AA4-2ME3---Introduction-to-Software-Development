b1dee35928c3bb44cbf2723f293ef9ba2bbaed65
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-29 19:19:07 -0400

    final implementation

--- A3/src/ProgramT.java
+++ A3/src/ProgramT.java
@@ -2,26 +2,36 @@ package src;
 
 import java.util.HashSet;
 
-public class ProgramT{
+public class ProgramT extends HashSet<CourseT> implements Measures{
 
 	private HashSet<CourseT> s;
 
 	public double[] measures(){
-		throw new UnsupportedOperationException;
+		throw new UnsupportedOperationException("UnsupportedOperationException");
 	}
 
-	public double[] measures(IndicatorT int){
-		throw ne UnsupportedOperationException
+	public double[] measures(IndicatorT ind){
+		throw new UnsupportedOperationException("UnsupportedOperationException");
 	}
 
-	public double[] measurs(AttributeT att){
-		double[] arr1 = {0,0,0,0};
-		for (int i = 0; i < s.size(); i++){
-			double[] arr2 = measures(att)[i].measures();
-			double[] measureInd = sumMeas(arr1, arr2);
-			arr1 = measureInd;					
+	public double[] measures(AttributeT att){
+		double[] array = {0,0,0,0};
+		for (CourseT i : this){
+			array = sumMeas(array, i.measures(att));
 		}
-		Services.normal(measureInd);
+		return Services.normal(array);
+	}
+
+
+	private double[] sumMeas(double[] a, double[] b){
+		double[] sum = new double[a.length];
+		for (int i = 0; i < a.length; i++){
+			sum[i] = a[i];
+		}
+		for (int j = 0; j < b.length; j++){
+			sum[j] = sum[j] + b[j];
+		}
+		return sum;
 	}
 }
 
