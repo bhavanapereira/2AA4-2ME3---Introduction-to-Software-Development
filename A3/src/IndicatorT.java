25fa355ae0e37be594a6695594a35f629d907476
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-29 16:19:42 -0400

    Implementation of Indicator Class

--- A3/src/IndicatorT.java
+++ A3/src/IndicatorT.java
@@ -1,1 +1,9 @@
 package src;
+
+
+enum IndicatorT{
+	math, specEngKnow, assumpt, suitableFund, recogTheory,
+	modelSelect, estOutcomes, desProcess, desPrinciples, openEnded,
+	ideaGeneration, healthSafety, standards, tools, engInSoc, awarePEO
+}
+		
