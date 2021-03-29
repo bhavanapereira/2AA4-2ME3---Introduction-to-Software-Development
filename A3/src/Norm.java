d6449dfdffb617137a578fe342f3ae710f95ace9
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-29 16:22:05 -0400

    Implementation of Norm abstract object

--- A3/src/Norm.java
+++ A3/src/Norm.java
@@ -1,1 +1,40 @@
 package src;
+
+
+public class Norm{
+
+	private static boolean normLOs;
+	private static boolean normInd;
+	private static boolean normAtt;
+
+	public static void setNorms(boolean nLos, boolean nInd, boolean nAtt){
+		normLOs = nLos;
+		normInd = nInd;
+		normAtt = nAtt;
+	}
+
+	public static boolean getNLOs(){
+		return normLOs;
+	}
+
+	public static boolean getNInd(){
+		return normInd;
+	}
+
+	public static boolean getNAtt(){
+		return normAtt;
+	}
+
+	public static void setNLOs(boolean nLOs){
+		normLOs = nLOs;
+	}
+
+	public static void setNInd(boolean nInd){
+		normInd = nInd;
+	}
+
+	public static void setNAtt(boolean nAtt){
+		normAtt = nAtt;
+	}
+
+}
