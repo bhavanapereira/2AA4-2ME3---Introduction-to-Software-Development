de257b63127f531069d13ce6de0efaf6c00b7372
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-29 16:20:18 -0400

    Implementation of Attribute Class

--- A3/src/AttributeT.java
+++ A3/src/AttributeT.java
@@ -1,2 +1,27 @@
 package src;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+public class AttributeT {
+
+	private String name;
+	private HashSet<IndicatorT> s;
+
+	public AttributeT (String attribName, IndicatorT[] indicators){
+		 this.name = attribName;
+		 s = new HashSet<IndicatorT>();
+		 for (int i = 0; i < indicators.length; i++){
+		 	s.add(indicators[i]);
+		 }
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public IndicatorT[] getIndicators(){
+		return s.toArray(new IndicatorT[0]);
+	}
+}
