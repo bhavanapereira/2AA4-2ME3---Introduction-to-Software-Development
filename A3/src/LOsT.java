2c94496d92653db1c8c004d396408ba49934e293
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-29 23:20:38 -0400

    safe commit

--- A3/src/LOsT.java
+++ A3/src/LOsT.java
@@ -10,8 +10,11 @@ public class LOsT implements Measures{
 
 
 	public LOsT(String topic, int nblw, int nmrg, int nmts, int nexc){
-		if (nblw <= 0 || nmrg <= 0 || nmts <= 0 || nexc <= 0){
-			throw new IllegalArgumentException("Illegal Argument Exception");
+		if (nblw < 0 || nmrg < 0 || nmts < 0 || nexc < 0){
+			throw new IllegalArgumentException("Illegal Argument Exception");				
+		}
+		if (nblw == 0 && nmrg == 0 && nmts == 0 && nexc == 0){
+			throw new IllegalArgumentException("Illegal Argument Exception");	
 		}
 		else{
 			this.name = topic;
